27b8ded0216b1210c4a4e7679fd2602b9c654ed9
Milozap <mzapolski96@gmail.com>
2025-11-24 18:37:15 +0100

    Add tests

--- src/test/java/com/mzap/gatewayservice/GatewayServiceApplicationTests.java
+++ src/test/java/com/mzap/gatewayservice/GatewayServiceApplicationTests.java
@@ -3,11 +3,14 @@ package com.mzap.gatewayservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
 class GatewayServiceApplicationTests {
 
     @Test
     void contextLoads() {
+        assertDoesNotThrow(() -> GatewayServiceApplication.main(new String[]{}));
     }
 
 }
